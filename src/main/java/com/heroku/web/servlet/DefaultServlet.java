6d679820572645bebdd54c5562558cf7f620ea45
dixanta <dixanta@gmail.com>
2018-05-15 19:14:33 +0545

    webapp runner

--- src/main/java/com/heroku/web/servlet/DefaultServlet.java
+++ src/main/java/com/heroku/web/servlet/DefaultServlet.java
@@ -9,6 +9,7 @@ import com.heroku.web.dao.impl.SkillDAOImpl;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
+import java.io.*;
 /**
  *
  * @author USER
@@ -17,12 +18,15 @@ import javax.servlet.http.*;
  
 public class DefaultServlet extends HttpServlet{
     private SkillDAO skillDAO=new SkillDAOImpl();
+    
     @Override
     protected void doGet(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("skills",skillDAO.getAll());
+           HttpServletResponse response) throws ServletException, IOException {
+        /*request.setAttribute("skills",skillDAO.getAll());
         request.getRequestDispatcher("/WEB-INF/views/index.jsp")
-                .forward(request, response);
+                .forward(request, response);*/
+            PrintWriter out=response.getWriter();
+            out.println("<h1>Hello World</h1>");
     }
     
     
